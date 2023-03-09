7b3f4fb4d5f11f8a5c3256e3811529b0c2dba0da
Kevin Benevides <61016019+kevinbsr@users.noreply.github.com>
2023-03-09 19:46:10 -0300

    Acesso ao DOM e ao Template com ViewChild

--- data-binding/src/app/output-property/output-property.component.ts
+++ data-binding/src/app/output-property/output-property.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'contador',
@@ -11,11 +11,15 @@ export class OutputPropertyComponent {
 
   @Output() mudouValor = new EventEmitter()
 
+  @ViewChild('campoInput', {static: false}) campoValorInput!: ElementRef;
+
   incrementa(){
-    this.valor++
+    console.log();
+    this.campoValorInput.nativeElement.value++
     this.mudouValor.emit({novoValor: this.valor})
   }
   decrementa(){
-    this.valor--
+    this.campoValorInput.nativeElement.value--
+    this.mudouValor.emit({novoValor: this.valor})
   }
 }
