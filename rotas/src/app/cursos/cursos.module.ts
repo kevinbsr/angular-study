a70091b6aa703694fa414e9e6a406aae71dbbe6e
kevinbsr <kb.kevinbenevides@gmail.com>
2023-03-16 23:20:07 -0300

    Criando um módulo de funcionalidade

---
+++ rotas/src/app/cursos/cursos.module.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { CursoDetalheComponent } from "./curso-detalhe/curso-detalhe.component";
+import { CursoNaoEncontradoComponent } from "./curso-nao-encontrado/curso-nao-encontrado.component";
+import { CursosComponent } from "./cursos.component";
+import { CursosService } from "./cursos.service";
+import { RouterModule } from "@angular/router";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [],
+  declarations: [
+    CursosComponent,
+    CursoDetalheComponent,
+    CursoNaoEncontradoComponent
+  ],
+  providers: [
+    CursosService
+  ],
+})
+export class CursosModule { }
